3a16f0ead575811b0e3865d2926ead102ab2b0aa
James Thistlewood <jamesthistlewood@gmail.com>
2018-11-15 17:20:50 +0000

    decustomisation from create-react-app defaults

---
+++ src/objects.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+class Clock extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {date: new Date()};
+	}
+
+	tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+	componentDidMount() {
+		this.timerID = setInterval(
+			() => this.tick(),
+			1000
+		);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timerID);
+	}
+
+	render() {
+		return (
+			<div>
+				<h2>It is {this.state.date.toLocaleTimeString()}</h2>
+			</div>
+		);
+	}
+}
+
+class Header extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div id="Header">
+				<h1>Header</h1>
+				<Clock />
+			</div>
+		);
+	}
+}
+
+
+export { Clock, Header };
+export default Clock;
